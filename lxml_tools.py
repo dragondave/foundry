26510c4d268e9fcaa9d9cd4cf83f5a57e2398008
Dragon Dave McKee <dragon@scraperwiki.com>
2019-07-16 18:53:39 +0100

    Changes

--- lxml_tools.py
+++ lxml_tools.py
@@ -1,9 +1,15 @@
 from urllib.parse import urljoin
 from youtube_dl import YoutubeDL
+import os
 import lxml.html
+import shutil
+
 PRESERVE = "preserve"      # attribute on a tag which is not to be changed
 URL_TAGS = ["src", "href"] # tag attributes that might be URLs
 GLOBE = u"\U0001F310"
+
+class NoVideoError(Exception):
+    pass
 
 def absolve(root, url):
     "Modifies an lxml tree to make all links absolute, except those marked with PRESERVE and local #"
@@ -17,15 +23,34 @@ def absolve(root, url):
                 continue
             tag.attrib[attr] = urljoin(url, tag.attrib[attr])
 
-def handle_youtube(tag):
+def handle_youtube(tag, path="."):
     attr, = [attr for attr in URL_TAGS if 'youtu' in tag.attrib.get(attr, "")]
     url = tag.attrib[attr]
-    ydl_opts = {"quiet": True}
+    ydl_opts = {"quiet": True,
+                "outtmpl": "X",
+                "format": "134/135/136/137/133+140"}
+    if "results" in url: return None
+
     with YoutubeDL(ydl_opts) as ydl:
-        filename = ydl.download([url])
+        print ("**youtube: ", url)
+        info_dict = ydl.extract_info(url, download=True)
+        filename = path + "/" + (info_dict["id"]+".mp4")
+        try:
+            shutil.copy("X.mp4", filename)
+            os.remove("X.mp4")
+        except:
+            try:
+                shutil.copy("X", filename)
+                os.remove("X")
+            except:
+                pass
+
+    if not os.path.exists(filename):
+        raise NoVideoError()
+
     print (repr(filename), attr)
     tag.attrib[attr] = filename
-    return filename
+    return str(filename)
 
 def globalise(tag):
     """
